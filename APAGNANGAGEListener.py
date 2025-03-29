985858979ba429ba65a132091bdedb253355c44e
Anton Appel <antonappel@legolas-2.local>
2025-03-29 12:55:26 +0100

    recompilation de antlr

--- APAGNANGAGEListener.py
+++ APAGNANGAGEListener.py
@@ -1,6 +1,6 @@
-# Generated from APAGNANGAGE.g4 by ANTLR 4.10.1
+# Generated from APAGNANGAGE.g4 by ANTLR 4.13.2
 from antlr4 import *
-if __name__ is not None and "." in __name__:
+if "." in __name__:
     from .APAGNANGAGEParser import APAGNANGAGEParser
 else:
     from APAGNANGAGEParser import APAGNANGAGEParser
