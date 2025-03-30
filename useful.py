59a67247ecf6694530531a03c8c4ea3988c17866
Bananasmoothii <45853225+bananasmoothii@users.noreply.github.com>
2025-03-30 15:14:12 +0200

    fix special chars

--- useful.py
+++ useful.py
@@ -2,6 +2,7 @@ import io
 import sys
 from io import StringIO
 
+
 #############################
 # Librairie des trucs utils #
 #############################
@@ -10,27 +11,28 @@ from io import StringIO
 def readfile(filename: str) -> str:
     # Bien plus pratique d'avoir qu'une seul fonction
     # plutôt que deux lignes et une indentation ...
-    with open(filename, "r") as file:
+    with open(filename, "r", encoding="UTF-8") as file:
         return file.read()
-    
-    
-def escapeWhitespace(s:str, escapeSpaces:bool):
+
+
+def escapeWhitespace(s: str, escapeSpaces: bool):
     with StringIO() as buf:
         for c in s:
-            if c==' ' and escapeSpaces:
+            if c == ' ' and escapeSpaces:
                 buf.write('\u00B7')
-            elif c=='\t':
+            elif c == '\t':
                 buf.write("\\t")
-            elif c=='\n':
+            elif c == '\n':
                 buf.write("\\n")
-            elif c=='\r':
+            elif c == '\r':
                 buf.write("\\r")
             else:
                 buf.write(c)
         return buf.getvalue()
 
+
 class ApagnanStringIO(io.StringIO):
-    def __init__(self, old_io: io.StringIO, file:str, *args, **kwargs):
+    def __init__(self, old_io: io.StringIO, file: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.__old_io = old_io
         self.__file = open(file, "w+")
@@ -44,7 +46,6 @@ class ApagnanStringIO(io.StringIO):
         self.__old_io.close()
         self.__file.close()
         super().close()
-
 
 
 def setup_print(logfile: str):
